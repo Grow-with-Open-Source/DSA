9519e8b899eda062dbebf17b29045013ec223685
rahulch-1 <rahulchereddy@gmail.com>
2023-10-26 13:37:04 +0530

    new commit

--- Solved-Problems/Backspace_String_Compare/Main.java
+++ Solved-Problems/Backspace_String_Compare/Main.java
@@ -1,57 +1,52 @@
 import java.util.Stack;
 
-
 class Main {
 
     public static boolean backspaceCompare(String s, String t) {
-
-        Stack<Character> s1 = new Stack<Character>();
-        Stack<Character> s2 = new Stack<Character>();
+        Stack<Character> s1 = new Stack<>();
+        Stack<Character> s2 = new Stack<>();
         
         for (char ch : s.toCharArray()) {
-
-            if(ch == '#') {
-                if(!s1.isEmpty()) {
+            if (ch == '#') {
+                if (!s1.isEmpty()) {
                     s1.pop();
                 }   
                 continue;
             }
             s1.push(ch);
-
         }
         
         for (char ch : t.toCharArray()) {
-            if(ch == '#') {
-                if(!s2.isEmpty()) { 
+            if (ch == '#') {
+                if (!s2.isEmpty()) {
                     s2.pop();
                 }
                 continue;
-                
             }
             s2.push(ch);
         }
         
-        String res1 = "";
-        String res2 = "";
+        StringBuilder res1 = new StringBuilder();
+        StringBuilder res2 = new StringBuilder();
         
-        while(!s1.isEmpty()) {
-            res1 += s1.pop();
+        while (!s1.isEmpty()) {
+            res1.append(s1.pop());
         }
         
-        while(!s2.isEmpty()) {
-            res2 += s2.pop();
+        while (!s2.isEmpty()) {
+            res2.append(s2.pop());
         }
         
-        return res1.equals(res2);
+        return res1.toString().equals(res2.toString());
     }
 
     public static void main(String[] args) {
         String s = "ab#c";
         String t = "ad#c";
-        System.out.println(backspaceCompare(s,t));
+        System.out.println(backspaceCompare(s, t));
     }
-    
 }
+
 
 // LEETCODE SOLUTION
 
