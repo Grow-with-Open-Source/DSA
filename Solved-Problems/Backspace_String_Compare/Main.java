f612deb7ef44a6f579d8b9833c937143536c92e6
rahulch-1 <rahulchereddy@gmail.com>
2023-10-26 10:36:41 +0530

    Resolved linter checks

--- Solved-Problems/Backspace_String_Compare/Main.java
+++ Solved-Problems/Backspace_String_Compare/Main.java
@@ -1,23 +1,32 @@
-import java.util.*;
+import java.util.Stack;
+
+
 class Main {
+
     public static boolean backspaceCompare(String s, String t) {
+
         Stack<Character> s1 = new Stack<Character>();
         Stack<Character> s2 = new Stack<Character>();
         
-        for(char ch : s.toCharArray()){
-            if(ch == '#'){
-                if(!s1.isEmpty())
+        for (char ch : s.toCharArray()) {
+
+            if(ch == '#') {
+                if(!s1.isEmpty()) {
                     s1.pop();
+                }   
                 continue;
             }
             s1.push(ch);
+
         }
         
-        for(char ch : t.toCharArray()){
-            if(ch == '#'){
-                if(!s2.isEmpty())
+        for (char ch : t.toCharArray()) {
+            if(ch == '#') {
+                if(!s2.isEmpty()) { 
                     s2.pop();
+                }
                 continue;
+                
             }
             s2.push(ch);
         }
@@ -25,19 +34,23 @@ class Main {
         String res1 = "";
         String res2 = "";
         
-        while(!s1.isEmpty())
+        while(!s1.isEmpty()) {
             res1 += s1.pop();
+        }
         
-        while(!s2.isEmpty())
+        while(!s2.isEmpty()) {
             res2 += s2.pop();
+        }
         
         return res1.equals(res2);
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         String s = "ab#c";
         String t = "ad#c";
         System.out.println(backspaceCompare(s,t));
     }
+    
 }
 
 // LEETCODE SOLUTION
