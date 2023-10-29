a05e5cf7a2e800a1f24c83f1ae6e2a95952f4c58
rahulch-1 <rahulchereddy@gmail.com>
2023-10-29 14:46:09 +0530

    made changes in program file

--- Solved-Problems/Backspace_String_Compare/Main.java
+++ Solved-Problems/Backspace_String_Compare/Main.java
@@ -1,3 +1,12 @@
+
+//Problem Name: 844.Backspace String Compare
+
+//Approach: Using Stacks
+
+//Difficulty: Easy
+
+//Link: https://leetcode.com/problems/backspace-string-compare/
+
 import java.util.Stack;
 
 class Main {
