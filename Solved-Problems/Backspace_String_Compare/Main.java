38118d5307563432e109de49f583e5152084a558
Shamith <shamith301102@gmail.com>
2023-10-29 13:04:56 +0530

    Manually Fixing Linting Issue

--- Solved-Problems/Backspace_String_Compare/Main.java
+++ Solved-Problems/Backspace_String_Compare/Main.java
@@ -1,48 +1,45 @@
 import java.util.Stack;
 
 class Main {
-  public static boolean backspaceCompare(
-      String s, String t) Stack<Character> s1 = new Stack<>() Stack<Character> s2 = new Stack<>();
+  public static boolean backspaceCompare(String s, String t) {
+    Stack<Character> s1 = new Stack<Character>();
+    Stack<Character> s2 = new Stack<Character>();
 
-  for (char ch : s.toCharArray()) {
-    if (ch == '#') {
-      if (!s1.isEmpty()) {
-        s1.pop();
+    for(char ch : s.toCharArray()) {
+      if(ch == '#'){
+        if(!s1.isEmpty())
+          s1.pop();
+        continue;
       }
-      continue;
+      s1.push(ch);
     }
-    s1.push(ch);
-  }
 
-  for (char ch : t.toCharArray()) {
-    if (ch == '#') {
-      if (!s2.isEmpty()) {
-        s2.pop();
-      }
-      continue;
+    for(char ch : t.toCharArray()) {
+      if(ch == '#'){
+        if(!s2.isEmpty())
+          s2.pop();
+        continue;
+        }
+        s2.push(ch);
     }
-    s2.push(ch);
+        
+    String res1 = "";
+    String res2 = "";
+        
+    while(!s1.isEmpty())
+      res1 += s1.pop();
+        
+    while(!s2.isEmpty())
+      res2 += s2.pop();
+        
+    return res1.equals(res2);
   }
-
-  StringBuilder res1 = new StringBuilder();
-  StringBuilder res2 = new StringBuilder();
-
-  while (!s1.isEmpty()) {
-    res1.append(s1.pop());
+  
+  public static void main(String[] args) {
+    String s = "ab#c";
+    String t = "ad#c";
+    System.out.println(backspaceCompare(s,t));
   }
-
-  while (!s2.isEmpty()) {
-    res2.append(s2.pop());
-  }
-
-  return res1.toString().equals(res2.toString());
-}
-
-public static void main(String[] args) {
-  String s = "ab#c";
-  String t = "ad#c";
-  System.out.println(backspaceCompare(s, t));
-}
 }
 
 // LEETCODE SOLUTION
@@ -52,7 +49,7 @@ public static void main(String[] args) {
 //     public boolean backspaceCompare(String s, String t) {
 //         Stack<Character> s1 = new Stack<Character>();
 //         Stack<Character> s2 = new Stack<Character>();
-
+        
 //         for(char ch : s.toCharArray()){
 //             if(ch == '#'){
 //                 if(!s1.isEmpty())
@@ -61,7 +58,7 @@ public static void main(String[] args) {
 //             }
 //             s1.push(ch);
 //         }
-
+        
 //         for(char ch : t.toCharArray()){
 //             if(ch == '#'){
 //                 if(!s2.isEmpty())
@@ -70,16 +67,16 @@ public static void main(String[] args) {
 //             }
 //             s2.push(ch);
 //         }
-
+        
 //         String res1 = "";
 //         String res2 = "";
-
+        
 //         while(!s1.isEmpty())
 //             res1 += s1.pop();
-
+        
 //         while(!s2.isEmpty())
 //             res2 += s2.pop();
-
+        
 //         return res1.equals(res2);
 //     }
 // }
