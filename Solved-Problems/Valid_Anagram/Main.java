4ae2e8dc6060359326230c7073e641b2129019e5
shreyasrajiv327 <shreyasrajiv327@gmail.com>
2024-08-05 16:54:09 +0530

    fixed the indentation

--- Solved-Problems/Valid_Anagram/Main.java
+++ Solved-Problems/Valid_Anagram/Main.java
@@ -3,18 +3,16 @@ import java.util.Map;
 
 class Main {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length())
-        {
+        if (s.length() != t.length()) {
             return false;
         }
         
         Map<Character, Integer> countS = new HashMap<>();
         Map<Character, Integer> countT = new HashMap<>();
 
-        for(int i = 0;i<s.length();i++)
-        {
+        for (int i = 0; i < s.length(); i++) {
             char c1 = s.charAt(i);
-            countS.put(c1,countS.getOrDefault(c1,0)+1);
+            countS.put(c1, countS.getOrDefault(c1, 0) + 1);
         }
     
         for (int i = 0; i < t.length(); i++) {
@@ -22,13 +20,12 @@ class Main {
             countT.put(c2, countT.getOrDefault(c2, 0) + 1);
         }
         
-         for(char c : countS.keySet())
-         {
-            if(!countT.containsKey(c) || !countT.get(c).equals(countS.get(c)))
-            {
+        for (char c : countS.keySet()) {
+            if (!countT.containsKey(c) || !countT.get(c).equals(countS.get(c))) {
                 return false;
             }
-         }
+        }
+        
         return true;
     }
 }
