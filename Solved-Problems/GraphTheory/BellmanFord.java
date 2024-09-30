9bc51ec6764b1a7f0c41897560ff8c1c884a37a1
DBasu2610 <basudebadrita94@gmail.com>
2024-09-30 13:58:42 +0530

    modified

--- Solved-Problems/GraphTheory/BellmanFord.java
+++ Solved-Problems/GraphTheory/BellmanFord.java
@@ -1,32 +1,31 @@
-package DSA.DSA.SolvedProblems.GraphTheory;
-
 import java.util.ArrayList;
 
 public class BellmanFord {
     public static void main(String[] args) {
         int vertex = 5;
+        @SuppressWarnings("unchecked")
         ArrayList<Edge>[] graph = new ArrayList[vertex];
-        
 
         create(graph);
         bf(graph, 0, vertex);
 
     }
 
-    static class  Edge {
+    static class Edge {
         int src;
         int dest;
         int weight;
+
         public Edge(int s, int d, int w) {
             this.src = s;
             this.dest = d;
             this.weight = w;
-        }      
+        }
     }
 
     public static void create(ArrayList<Edge> graph[]) {
         for (int i = 0; i < graph.length; i++) {
-            graph[i] = new ArrayList<>();     
+            graph[i] = new ArrayList<>();
         }
         graph[0].add(new Edge(0, 1, 2));
         graph[0].add(new Edge(0, 2, 4));
@@ -44,22 +43,22 @@ public class BellmanFord {
     public static void bf(ArrayList<Edge> graph[], int src, int V) {
         int[] dis = new int[V];
         for (int i = 0; i < V; i++) {
-            if(i!=src) {
+            if (i != src) {
                 dis[i] = Integer.MAX_VALUE;
             }
         }
 
-        for(int k=0;k<V-1; k++) {
-            for(int j=0;j<V;j++) {
+        for (int k = 0; k < V - 1; k++) {
+            for (int j = 0; j < V; j++) {
                 for (int i = 0; i < graph[i].size(); i++) {
                     Edge e = graph[j].get(i);
-                    int u  = e.src;
+                    int u = e.src;
                     int v = e.dest;
 
-                    if( dis[u]<Integer.MAX_VALUE && dis[u] + e.weight< dis[v]) {
-                        dis[v]=dis[u] + e.weight;
+                    if (dis[u] < Integer.MAX_VALUE && dis[u] + e.weight < dis[v]) {
+                        dis[v] = dis[u] + e.weight;
                     }
-                    
+
                 }
             }
 
@@ -67,8 +66,7 @@ public class BellmanFord {
 
         for (int i = 0; i < dis.length; i++) {
             System.err.println(dis[i] + " ");
-            
+
         }
     }
 }
-
--- Solved-Problems/GraphTheory/Dijkstra.java
+++ Solved-Problems/GraphTheory/Dijkstra.java
@@ -1,4 +1,3 @@
-package DSA.DSA.SolvedProblems.GraphTheory;
 
 import java.util.ArrayList;
 import java.util.PriorityQueue;
@@ -6,17 +5,19 @@ import java.util.PriorityQueue;
 public class Dijkstra {
     public static void main(String[] args) {
         int vertex = 6;
+        @SuppressWarnings("unchecked")
         ArrayList<Edge> graph[] = new ArrayList[vertex];
         create(graph);
 
         ShortestPath(graph, 0, vertex);
-        
+
     }
 
-    static class Edge{
+    static class Edge {
         int src;
         int dest;
         int weight;
+
         public Edge(int s, int d, int w) {
             this.src = s;
             this.dest = d;
@@ -25,30 +26,30 @@ public class Dijkstra {
 
     }
 
-
     public static void create(ArrayList<Edge> graph[]) {
         for (int i = 0; i < graph.length; i++) {
-            graph[i] = new ArrayList<Edge>();    
+            graph[i] = new ArrayList<Edge>();
         }
 
         graph[0].add(new Edge(0, 1, 2));
-        graph[0].add(new Edge(0,2,4));
+        graph[0].add(new Edge(0, 2, 4));
 
-        graph[1].add(new Edge(1,3,7));
-        graph[1].add(new Edge(1,2,1));
+        graph[1].add(new Edge(1, 3, 7));
+        graph[1].add(new Edge(1, 2, 1));
 
-        graph[2].add(new Edge(2,4,3));
+        graph[2].add(new Edge(2, 4, 3));
 
-        graph[3].add(new Edge(3,5,1));
+        graph[3].add(new Edge(3, 5, 1));
 
-        graph[4].add(new Edge(4,3,2));
-        graph[4].add(new Edge(4,5,5));
- 
+        graph[4].add(new Edge(4, 3, 2));
+        graph[4].add(new Edge(4, 5, 5));
+
     }
 
     public static class Pair implements Comparable<Pair> {
         int node;
         int distance;
+
         public Pair(int n, int d) {
             this.node = n;
             this.distance = d;
@@ -56,7 +57,7 @@ public class Dijkstra {
 
         @Override
         public int compareTo(Pair p2) {
-            return this.distance- p2.distance;
+            return this.distance - p2.distance;
 
         }
     }
@@ -66,36 +67,34 @@ public class Dijkstra {
         boolean vis[] = new boolean[V];
         int dist[] = new int[V];
         for (int i = 0; i < V; i++) {
-            if(i!=src) {
+            if (i != src) {
                 dist[i] = Integer.MAX_VALUE;
             }
         }
         pq.add(new Pair(0, 0));
 
-
-        while(!pq.isEmpty()) {
+        while (!pq.isEmpty()) {
             Pair curr = pq.remove();
-            if(!vis[curr.node]) {
+            if (!vis[curr.node]) {
                 vis[curr.node] = true;
                 for (int i = 0; i < graph[curr.node].size(); i++) {
                     Edge e = graph[curr.node].get(i);
                     int u = e.src;
                     int v = e.dest;
 
-                    if(dist[u]+ e.weight< dist[v]) {
-                        dist[v] = dist[u]+ e.weight;
+                    if (dist[u] + e.weight < dist[v]) {
+                        dist[v] = dist[u] + e.weight;
                         pq.add(new Pair(v, dist[v]));
                     }
-                    
+
                 }
             }
         }
 
         for (int i = 0; i < V; i++) {
-            System.out.print(dist[i]+ " ");   
+            System.out.print(dist[i] + " ");
         }
 
     }
-    
-}
 
+}
