463b939e33e6964858b683de2df342896b42ef28
iamwatchdogs <shamith301102@gmail.com>
2024-10-18 19:24:39 +0530

    Updated scripts
    Updated the logging scripts to properly log contributions made to
    files where .github parent directory. And to reflect those changes
    to deployed version, the markdown & html conversion script were also
    updated.
    
    Also added docstrings to each functions within all the scripts.

--- .github/scripts/update_contributors_log.py
+++ .github/scripts/update_contributors_log.py
@@ -15,56 +15,100 @@ This script requires following environment variables:
   > GitHub action variable: ${{ github.event.pull_request.number }}
 '''
 
-def get_contribution_title(CURRENT_PR):
-	
+
+def get_project_title(pr_data):
+	"""
+	Determines the project title based on the file paths in the pull request data.
+
+	Args:
+			pr_data (dict): The pull request data containing file paths.
+
+	Returns:
+			str: The project title derived from the directory name in the file path.
+						Returns 'root' if changes are made in the root of the repository.
+						Special cases include '{workflows}', '{scripts}', and '{others}'
+						for certain paths within the '.github' directory.
+
+    """
+
 	# Setting default value
-	contribution_title = 'root'
-	path = contribution_title
+	project_title = 'root'
 
 	# Iterating through the "files" list
-	for files in CURRENT_PR["files"]:
-		if '/' in files["path"]:
-			contribution_title = files["path"]
-			path = contribution_title
+	for i in pr_data["files"]:
+		if '/' in i["path"]:
+			project_title = i["path"]
 			break
 
-	# If we find a directory
-	if contribution_title != 'root':
-		splitted_title = contribution_title.split('/')
-		contribution_title = splitted_title[-2] if '.' in contribution_title else splitted_title[-1]
+	# changes are made in the root of repo
+	if project_title == 'root':
+		return project_title
 
-	return (contribution_title, path)
+	if '.github/workflows' in project_title:
+		project_title = '{workflows}'
+	elif '.github/scripts' in project_title:
+		project_title = '{scripts}'
+	elif '.github' in project_title:
+		project_title = '{others}'
+	else:
+		project_title = project_title.split('/')[0]  # directory name
 
-def get_contributor_name(CURRENT_PR): 
-	return CURRENT_PR["author"]["login"]
+	return project_title
 
-def get_core_type(CONTRIBUTION_TITLE, USED_PATH):
-	return USED_PATH.split('/')[0] if CONTRIBUTION_TITLE != 'root' else 'Repo'
 
-def get_specificity(CONTRIBUTION_TITLE, USED_PATH):
-	return USED_PATH.split('/')[1] if CONTRIBUTION_TITLE != 'root' else 'Maintenance'
+def get_contributor_name(pr_data):
+	"""
+	Retrieves the username of the contributor who made the pull request.
 
-def get_demo_path(CURRENT_PR, CONTRIBUTION_TITLE, CORE_TYPE, SPECIFICITY):
+	Args:
+		pr_data (dict): The pull request data containing the author's username.
+
+	Returns:
+		str: The username of the contributor.
+	"""
+	return pr_data["author"]["login"]
+
+
+def get_demo_path(pr_data):
+	"""
+	Retrieves the demo path for the pull request.
+
+	Args:
+		pr_data (dict): The pull request data containing information about the pull request.
+
+	Returns:
+		str: The demo path of the pull request.
+	"""
 
 	# Getting required values
 	REPO_NAME = os.environ.get('REPO_NAME')
+	PROJECT_NAME = get_project_title(pr_data)
 
 	# Handling a base case
-	if CONTRIBUTION_TITLE == 'root':
+	if PROJECT_NAME == 'root':
 		return f'https://github.com/{REPO_NAME}/'
 
+	url_path = PROJECT_NAME
+
+	# Setting custom path for workflow maintance
+	SPECIAL_CASES = ['{workflows}', '{scripts}', '{others}']
+	if PROJECT_NAME in SPECIAL_CASES:
+		url_path = '.github'
+		if PROJECT_NAME in SPECIAL_CASES[:2]:
+			url_path += f'/{PROJECT_NAME[1:-1]}'
+
 	# Setting default value
-	demo_path = f'https://github.com/{REPO_NAME}/tree/main/{CORE_TYPE}/{SPECIFICITY}' 
+	demo_path = f'https://github.com/{REPO_NAME}/tree/main/{url_path}'
 	found_required_path = False
 
 	# Iterating through the "files" list
-	for files in CURRENT_PR["files"]:
-		path = files["path"]
+	for file_data in pr_data["files"]:
+		path = file_data["path"]
 		if "index.html" in path:
 			demo_path = path
 			found_required_path = True
 			break
-		elif  path.lower().endswith('index.md') or path.lower().endswith('readme.md'):
+		elif path.lower().endswith('index.md') or path.lower().endswith('readme.md'):
 			demo_path = path
 			found_required_path = True
 
@@ -78,31 +122,46 @@ def get_demo_path(CURRENT_PR, CONTRIBUTION_TITLE, CORE_TYPE, SPECIFICITY):
 
 	return demo_path
 
+
 def main():
-    
-    # Setting file paths
-	PR_DETAILS_FILE_PATH = 'pr.json'
-	CONTRIBUTION_LOG_FILE_PATH = '.github/data/contributors-log.json'
+	"""
+	Updates the contributors log file after a pull request has been merged.
+
+	This function is to be called in a GitHub Actions workflow after a pull request has been merged.
+	It reads the details of the current pull request from a JSON file, extracts the required information,
+	and updates the contributors log file accordingly.
+
+	The contributors log file is a JSON file that contains information about each contributor, including
+	their name, the number of the pull request they contributed to, and the path to their project.
+
+	The function dumps the data into the log file and outputs a success message upon completion.
+
+	Args:
+		None
+
+	Returns:
+		None
+	"""
+
+	# Setting required file paths
+	CURRENT_PR_DETAILS_PATH = 'pr.json'
+	CONTRIBUTORS_LOG_PATH = '.github/data/contributors-log.json'
 
 	# Reading contents from the current pr
-	with open(PR_DETAILS_FILE_PATH, 'r') as json_file:
-		CURRENT_PR = json.load(json_file)
-	
+	with open(CURRENT_PR_DETAILS_PATH, 'r') as json_file:
+		current_pr = json.load(json_file)
+
 	# Getting required value for update
-	CONTRIBUTION_TITLE, USED_PATH = get_contribution_title(CURRENT_PR)
-	CONTRIBUTOR_NAME = get_contributor_name(CURRENT_PR)
-	CORE_TYPE = get_core_type(CONTRIBUTION_TITLE, USED_PATH)
-	SPECIFICITY = get_specificity(CONTRIBUTION_TITLE, USED_PATH)
+	PROJECT_TITLE = get_project_title(current_pr)
+	CONTRIBUTOR_NAME = get_contributor_name(current_pr)
 	PR_NUMBER = os.environ.get('PR_NUMBER')
-	DEMO_PATH = get_demo_path(CURRENT_PR, CONTRIBUTION_TITLE, CORE_TYPE, SPECIFICITY)
+	DEMO_PATH = get_demo_path(current_pr)
 
 	# Creating a new dict objects for JSON conversion
 	existing_data = None
 	new_data = {
-		CONTRIBUTION_TITLE: {
+		PROJECT_TITLE: {
 			"contributor-name": [CONTRIBUTOR_NAME],
-			"core": CORE_TYPE,
-			"specificity": SPECIFICITY,
 			"pull-request-number": [PR_NUMBER],
 			"demo-path": DEMO_PATH
 		}
@@ -110,18 +169,18 @@ def main():
 
 	# Processing the data dumps
 	operation_name = None
-	if os.path.exists(CONTRIBUTION_LOG_FILE_PATH):
+	if os.path.exists(CONTRIBUTORS_LOG_PATH):
 
 		# Reading existing Log file
-		with open(CONTRIBUTION_LOG_FILE_PATH, 'r') as json_file:
+		with open(CONTRIBUTORS_LOG_PATH, 'r') as json_file:
 			existing_data = json.load(json_file)
 
 		# performing updation or addition based on `PROJECT_TITLE`
-		if CONTRIBUTION_TITLE in existing_data:
-			if CONTRIBUTOR_NAME not in existing_data[CONTRIBUTION_TITLE]["contributor-name"]:
-				existing_data[CONTRIBUTION_TITLE]["contributor-name"].append(CONTRIBUTOR_NAME)
-			if PR_NUMBER not in existing_data[CONTRIBUTION_TITLE]["pull-request-number"]:
-				existing_data[CONTRIBUTION_TITLE]["pull-request-number"].append(PR_NUMBER)
+		if PROJECT_TITLE in existing_data:
+			if CONTRIBUTOR_NAME not in existing_data[PROJECT_TITLE]["contributor-name"]:
+				existing_data[PROJECT_TITLE]["contributor-name"].append(CONTRIBUTOR_NAME)
+			if PR_NUMBER not in existing_data[PROJECT_TITLE]["pull-request-number"]:
+				existing_data[PROJECT_TITLE]["pull-request-number"].append(PR_NUMBER)
 			operation_name = 'Updated'
 		else:
 			existing_data.update(new_data)
@@ -131,11 +190,12 @@ def main():
 		operation_name = 'Created'
 
 	# Dumping the data into log file
-	with open(CONTRIBUTION_LOG_FILE_PATH, 'w') as json_file:
+	with open(CONTRIBUTORS_LOG_PATH, 'w') as json_file:
 		json.dump(existing_data, json_file, indent=2)
 
 	# Output message
 	print(f'Successfully {operation_name} the log file')
 
+
 if __name__ == '__main__':
-    main()
+	main()
